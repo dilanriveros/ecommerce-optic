5bdfd5c1c97af6f9c3268d91a5fb692586cd2d16
Dilan Hernandez <dilanriveros2304@gmail.com>
2026-01-29 13:50:48 -0500

    fix: urls imagenes categorias strapi y typess

--- src/api/getCategoryProduct.tsx
+++ src/api/getCategoryProduct.tsx
@@ -1,25 +1,24 @@
 import { useEffect, useState } from "react"
 
-export function useGetCategories() {
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/categories?populate=*`
-    const [result, setResult] = useState<any[]>([]) // ← INICIALIZA COMO ARRAY VACÍO
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
+export function useGetCategoryProduct (slug: string | string[]){
+     const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?populate=*&filters[category][slug][$eq]=${slug}`
+        const [result, setResult] = useState(null)
+        const [loading, setLoading] = useState(true)
+        const [error, setError] = useState('')
+          useEffect (() => {
+            (async () => {
+                try {
+                   const res = await fetch (url) 
+                   const json = await res.json()
+                   setResult(json.data)
+                   setLoading (false)
+                } catch (error: any) {
+                    setError (error)
+                    setLoading (false)
+                    
+                }
+            })()
     
-    useEffect(() => {
-        (async () => {
-            try {
-                const res = await fetch(url) 
-                const json = await res.json()
-                // Asegúrate de que json.data sea un array
-                setResult(Array.isArray(json.data) ? json.data : [])
-                setLoading(false)
-            } catch (error: any) {
-                setError(error.message || "Error desconocido")
-                setLoading(false)
-            }
-        })()
-    }, [url])
-    
-    return { loading, result, error }
+          }, [url])
+          return {loading, result, error}
 }
